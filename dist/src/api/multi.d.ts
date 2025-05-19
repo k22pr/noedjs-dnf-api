55f060ed4f2664fec2df890579ce3a3ce37258bd
ksPr <h2x@pm.me>
2025-05-19 19:50:39 +0900

    publish : 1.0.4

--- dist/src/api/multi.d.ts
+++ dist/src/api/multi.d.ts
@@ -1,7 +1,7 @@
-import type * as Model from "../model";
+import type * as model from "../model";
 /**
  * 해당하는 아이템의 상세정보를 요청합니다.
  *
  * @param {string} itemId 검색할 아이템의 ID
  */
-export declare const items: (itemIdList: string[]) => Promise<Model.IDnfResponse<Model.item.IDetail[]>>;
+export declare const items: (itemIdList: string[]) => Promise<model.IDnfResponse<model.IRows<model.item.IDetail>>>;
